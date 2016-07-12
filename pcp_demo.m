6e70d981d97ded2d9f4e7729155331b73df1a7b1
Eric V. Strobl <ericvonstrobl@gmail.com>
2016-07-12 13:07:14 -0400

    Update pcp_demo.m

--- pcp_demo.m
+++ pcp_demo.m
@@ -8,6 +8,7 @@ load DAG_graph1.mat
 %load DAG_graph4.mat
 %load DAG_graph5.mat
 
+
 d = size(graph,2);
 data=create_dataset_dag(graph,1000,randn(d,d));
 
@@ -15,11 +16,11 @@ data=create_dataset_dag(graph,1000,randn(d,d));
 %get pdag with raw p-values using Spearman's rho
 [pdag,p_val,IDs] = PC_with_pval(@rho_test_PC, [], [], size(data,2),data);
 
-%get alpha_star using a given FDR level q
+%control the FDR at a given FDR level q
 q=0.10;
 alpha_star = get_alpha_star(p_val, IDs, q)
 
-%approximate FDR for a given graph using a given alpha_threshold
+%estimate the FDR with a given alpha_threshold
 alpha=0.05;
 FDR_est=get_FDR(p_val,IDs,alpha)
 
@@ -29,11 +30,11 @@ FDR_est=get_FDR(p_val,IDs,alpha)
 %get skeleton with raw p-values
 [pdag_sk, sep_sk, p_val_sk, IDs_sk] = stable_skeleton_discovery(@rho_test_PC, [], [], size(data,2), data);
 
-%get alpha_star using a given FDR level q
+%control the FDR at a given FDR level q
 q=0.10;
 alpha_star = get_alpha_star(p_val_sk, IDs_sk, q)
 
-%approximate FDR for a given graph using a given alpha_threshold
+%estimate the FDR with a given alpha_threshold
 alpha=0.05;
 FDR_est=get_FDR(p_val_sk,IDs_sk,alpha)
 
