b1726f2298d59f88d42ec3cfeb2d1768e7027c58
Eric V. Strobl <ericvonstrobl@gmail.com>
2016-07-12 12:25:01 -0400

    Add files via upload

--- pcp_demo.m
+++ pcp_demo.m
@@ -1,13 +1,13 @@
 clear all
 clc
 
-
 %load a DAG, generate a Gaussian dataset
 load DAG_graph1.mat
+
 d = size(graph,2);
 data=create_dataset_dag(graph,1000,randn(d,d));
 
-%get pdag with raw p-values using Spearman's rho test
+%get pdag with raw p-values using Spearman's rho
 [pdag,p_val,IDs] = PC_with_pval(@rho_test_PC, [], [], size(data,2),data);
 
 %get alpha_star using a given FDR level q
@@ -18,4 +18,8 @@ FDR_est=get_FDR(p_val,IDs,0.05);
 
 %get FDR and FWER corrected p-values
 [FDR_p_val,FWER_p_val]=correct_p_values(p_val,IDs);
+
+
+
+
 
