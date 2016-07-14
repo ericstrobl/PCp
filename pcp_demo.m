f98dbf837f19163b5e5e09030c8dc2f2b237ee7b
Eric V. Strobl <ericvonstrobl@gmail.com>
2016-07-13 20:58:42 -0400

    Add files via upload

--- pcp_demo.m
+++ pcp_demo.m
@@ -3,40 +3,39 @@ clc
 
 %load a DAG, generate a Gaussian dataset
 load DAG_graph1.mat
-%load DAG_graph2.mat
-%load DAG_graph3.mat
-%load DAG_graph4.mat
-%load DAG_graph5.mat
-
+% load DAG_graph2.mat
+% load DAG_graph3.mat
+% load DAG_graph4.mat
+% load DAG_graph5.mat
 
 d = size(graph,2);
-data=create_dataset_dag(graph,5000,randn(d,d));
+data=create_dataset_dag(graph,10000,randn(d,d));
 
 %%
-%get pdag with raw p-values using Spearman's rho
-[pdag,p_val,IDs] = PC_with_pval(@rho_test_PC, [], [], size(data,2),data);
+%get pdag with raw p-values using Fizher's z test
+[pdag,p_val,IDs] = PC_with_pval(@lin_test_PC, [], [], size(data,2),size(data,1),data);
 
 %control the FDR at a given FDR level q
-q=0.05;
-alpha_star = get_alpha_star(p_val, IDs, q)
+q=0.1;
+[pdag_adj,alpha_star] = control_FDR(pdag,p_val, IDs, q)
 
 %estimate the FDR at a given alpha threshold
 alpha=0.05;
-FDR_est=get_FDR(p_val,IDs,alpha)
+FDR_est=estimate_FDR(p_val,IDs,alpha)
 
 
 
 %%
 %get skeleton with raw p-values
-[pdag_sk, sep_sk, p_val_sk, IDs_sk] = stable_skeleton_discovery(@rho_test_PC, [], [], size(data,2), data);
+[pdag_sk, ~, p_val_sk, IDs_sk] = stable_skeleton_discovery(@lin_test_PC, [], [], size(data,2), size(data,1),data);
 
 %control the FDR at a given FDR level q
-q=0.05;
-alpha_star = get_alpha_star(p_val_sk, IDs_sk, q)
+q=0.1;
+[pdag_adj,alpha_star] = control_FDR(pdag_sk, p_val_sk, IDs_sk, q)
 
 %estimate the FDR at a given alpha threshold
 alpha=0.05;
-FDR_est=get_FDR(p_val_sk,IDs_sk,alpha)
+FDR_est=estimate_FDR(p_val_sk, IDs_sk, alpha)
 
 
 
