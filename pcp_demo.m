48c9008d480709f756d9ce852863c2f5b78bbbb6
Eric V. Strobl <ericvonstrobl@gmail.com>
2019-02-15 08:44:56 -0500

    Update pcp_demo.m

--- pcp_demo.m
+++ pcp_demo.m
@@ -1,21 +1,19 @@
 clear all
 clc
 
-%% load a DAG, generate a Gaussian dataset
+%% create a DAG, generate a Gaussian dataset
 
-load DAG_graph1.mat
-% load DAG_graph2.mat
-% load DAG_graph3.mat
-% load DAG_graph4.mat
-% load DAG_graph5.mat
+n=1000; %sample size
+d=20; %number of vertices
+en=2; %expected neighborhood size
+B=create_dag(en,d);
+data=create_dataset_dag(B>0,n,B);
 
-d = size(graph,2);
-data=create_dataset_dag(graph,10000,randn(d,d));
-
-%% running PC-p
+%% run PC-p
 
 %get pdag with raw p-values using Fizher's z test
-[pdag,p_val,IDs] = PC_with_pval(@lin_test_PC, [], [], size(data,2),size(data,1),data);
+C=corr(data);
+[pdag,p_val,IDs] = PC_with_pval(@gaussCItest, [], [], size(data,2),C,n);
 
 %control the FDR at a given FDR level q
 q=0.1;
@@ -24,20 +22,6 @@ q=0.1;
 %estimate the FDR at a given alpha threshold
 alpha=0.05;
 FDR_est = estimate_FDR(p_val,IDs,alpha)
-
-
-%% running PC-stable's skeleton discovery procedure
-
-%get skeleton with raw p-values
-[pdag_sk, ~, p_val_sk, IDs_sk] = stable_skeleton_discovery(@lin_test_PC, [], [], size(data,2), size(data,1),data);
-
-%control the FDR at a given FDR level q
-q=0.1;
-[pdag_adj,alpha_star] = control_FDR(pdag_sk, p_val_sk, IDs_sk, q)
-
-%estimate the FDR at a given alpha threshold
-alpha=0.05;
-FDR_est = estimate_FDR(p_val_sk, IDs_sk, alpha)
 
 
 
